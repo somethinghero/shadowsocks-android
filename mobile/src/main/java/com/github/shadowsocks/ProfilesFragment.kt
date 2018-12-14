4ce643983edee32f1c7f7996a0104251bf44209a
somethinghero <somethinghero@163.com>
2018-12-14 11:44:32 +0800

    remove ad

--- mobile/src/main/java/com/github/shadowsocks/ProfilesFragment.kt
+++ mobile/src/main/java/com/github/shadowsocks/ProfilesFragment.kt
@@ -49,9 +49,9 @@ import com.github.shadowsocks.utils.Action
 import com.github.shadowsocks.utils.datas
 import com.github.shadowsocks.utils.printLog
 import com.github.shadowsocks.widget.UndoSnackbarManager
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
+//import com.google.android.gms.ads.AdRequest
+//import com.google.android.gms.ads.AdSize
+//import com.google.android.gms.ads.AdView
 import net.glxn.qrgen.android.QRCode
 import org.json.JSONArray
 
@@ -123,7 +123,7 @@ class ProfilesFragment : ToolbarFragment(), Toolbar.OnMenuItemClickListener {
         private val text2 = itemView.findViewById<TextView>(android.R.id.text2)
         private val traffic = itemView.findViewById<TextView>(R.id.traffic)
         private val edit = itemView.findViewById<View>(R.id.edit)
-        private var adView: AdView? = null
+        //private var adView: AdView? = null
 
         init {
             edit.setOnClickListener {
@@ -171,28 +171,28 @@ class ProfilesFragment : ToolbarFragment(), Toolbar.OnMenuItemClickListener {
                 if (selectedItem === this) selectedItem = null
             }
 
-            var adView = adView
-            if (item.host == "198.199.101.152") {
-                if (adView == null) {
-                    val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                            ViewGroup.LayoutParams.WRAP_CONTENT)
-                    params.gravity = Gravity.CENTER_HORIZONTAL
-                    adView = AdView(context)
-                    adView.layoutParams = params
-                    adView.adUnitId = "ca-app-pub-9097031975646651/7760346322"
-                    adView.adSize = AdSize.FLUID
-                    val padding = context.resources.getDimensionPixelOffset(R.dimen.profile_padding)
-                    adView.setPadding(padding, 0, 0, padding)
-
-                    itemView.findViewById<LinearLayout>(R.id.content).addView(adView)
-
-                    // Load Ad
-                    val adBuilder = AdRequest.Builder()
-                    adBuilder.addTestDevice("B08FC1764A7B250E91EA9D0D5EBEB208")
-                    adView.loadAd(adBuilder.build())
-                    this.adView = adView
-                } else adView.visibility = View.VISIBLE
-            } else adView?.visibility = View.GONE
+//            var adView = adView
+//            if (item.host == "198.199.101.152") {
+//                if (adView == null) {
+//                    val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                            ViewGroup.LayoutParams.WRAP_CONTENT)
+//                    params.gravity = Gravity.CENTER_HORIZONTAL
+//                    adView = AdView(context)
+//                    adView.layoutParams = params
+//                    adView.adUnitId = "ca-app-pub-9097031975646651/7760346322"
+//                    adView.adSize = AdSize.FLUID
+//                    val padding = context.resources.getDimensionPixelOffset(R.dimen.profile_padding)
+//                    adView.setPadding(padding, 0, 0, padding)
+//
+//                    itemView.findViewById<LinearLayout>(R.id.content).addView(adView)
+//
+//                    // Load Ad
+//                    val adBuilder = AdRequest.Builder()
+//                    adBuilder.addTestDevice("B08FC1764A7B250E91EA9D0D5EBEB208")
+//                    adView.loadAd(adBuilder.build())
+//                    this.adView = adView
+//                } else adView.visibility = View.VISIBLE
+//            } else adView?.visibility = View.GONE
         }
 
         override fun onClick(v: View?) {
